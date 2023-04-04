facfac864d8c178bbbf5cd4e10d507ae641c9e17
aldinpramudya <aldinarielpramudya@gmail.com>
2023-04-04 15:55:41 +0700

    add detail user

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -52,4 +52,5 @@
     </tr>
     @endforeach
 </table>
+{{$book->links()}}
 @stop
--- resources/views/book/selamat.blade.php
+++ resources/views/book/selamat.blade.php
@@ -1,5 +1,33 @@
 @extends('master')
 
 @section('selamat')
-    <h1><center>Selamat Datang {{$user->name}}</center></h1>
+<h1>
+    <center>Selamat Datang {{$user->name}}</center>
+</h1>
+<div class="card-body">
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
+
+    {{ __('You are logged in!') }}
+    <table class="table table-responsive">
+        <tr>
+            <th>Username</th>
+            <th>:</th>
+            <td>{{$user->username}}</td>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>:</th>
+            <td>{{$user->name}}</td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <th>:</th>
+            <td>{{$user->email}}</td>
+        </tr>
+    </table>
+</div>
 @stop
